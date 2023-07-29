dccaf84fa3604f233fccf4fc9a0059c29056d5a3
nt-williams <ntwilliams.personal@gmail.com>
2023-07-29 16:51:11 -0400

    Dependency updates.

--- R/tidy.R
+++ R/tidy.R
@@ -2,24 +2,17 @@
 #' @export
 generics::tidy
 
-#' Tidy a(n) lmtp object
+#' Tidy a(n) hmtp object
 #'
-#' @param x A `lmtp` object produced by a call to [lmtp::lmtp_tmle()], [lmtp::lmtp_sdr()],
-#' [lmtp::lmtp_sub()], or [lmtp::lmtp_ipw()].
+#' @param x A `hmtp` object produced by a call to [lmtp::hmtp_tmle()].
 #' @param ... Unused, included for generic consistency only.
 #'
 #' @examples
 #' \donttest{
-#' a <- c("A1", "A2")
-#' nodes <- list(c("L1"), c("L2"))
-#' cens <- c("C1", "C2")
-#' y <- "Y"
-#' fit <- lmtp_tmle(sim_cens, a, y, time_vary = nodes, cens = cens, shift = NULL, folds = 2)
-#' tidy(fit)
 #' }
 #'
 #' @export
-tidy.lmtp <- function(x, ...) {
+tidy.hmtp <- function(x, ...) {
   out <- data.frame(estimator = x$estimator,
                     estimate = x$theta,
                     std.error = x$standard_error,
